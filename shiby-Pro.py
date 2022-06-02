e194b4733f0fd271348c29778b2c373f77ef7e79
Shoaibsafi2 <97215018+Shoaibsafi2@users.noreply.github.com>
2022-06-02 23:13:21 +0500

    Add files via upload

---
+++ shiby-Pro.py
@@ -0,0 +1,18 @@
+
+apt update && upgrade
+
+pkg install git
+
+pkg install python
+
+pip2 install requests
+
+pip2 install mechanize
+
+pip2 install requests bs4
+
+git clone https://github.com/shoaibsafi2/shiby-Pro.git
+
+cd shiby-Pro
+
+python shiby-Pro.py
